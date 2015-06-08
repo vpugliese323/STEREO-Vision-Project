99620fd8e87ab1497782ad219352a65fc595b923
vpugliese323 <vpugliese323@gmail.com>
2015-06-08 14:37:22 -0400

    Update RangeFinder.ino
    Code works!

--- RangeFinder.ino
+++ RangeFinder.ino
@@ -1,5 +1,6 @@
 //Range finder hardware control code
-//#include <Time.h>
+//Code modified from: http://www.instructables.com/id/Simple-Arduino-and-HC-SR04-Example/?ALLSTEPS
+//User: jsvester
 
 void setup()
 {
@@ -10,43 +11,13 @@ void setup()
 
 void loop()
 {
-  /*
-  //int hightime = digitalRead(2);//signal bouncing off object
-  //float distance = ((hightime)*(340))/2;//conversion to distance
-  //the below is something general to test the sensor
-  int hightime = pulseIn(2, HIGH);
-  //digitalWrite(4, HIGH);
-  delay(10);
-  Serial.println(hightime);
-  */
-  
-  long int instructions = 0;
-
-  //time_t secs1 = now();
-  //time_t secs2;
-  
-  //while (!digitalRead(2))//if pin 2 is not HIGH...
- /* while(1)
-  {
-    digitalWrite(4, HIGH);
-    delay(1);
-    instructions++;
-    //if (instructions >= 16000000)
-    if (instructions >= 16000000)
-    {
-      break;
-    }
-    if (digitalRead(2))
-    {
-      break;
-    }
-  }*/
-    //should yeild error of less than (+/-) 100 instructions/clock cycle
-    
-  int duration = pulseIn(2, HIGH);
-  digitalWrite(4, HIGH); 
-  delay(500);
-  //double milliseconds = instructions*(0.016);
+  long duration;
+  digitalWrite(4, LOW);
+  delayMicroseconds(3);//smallest reliable interval
+  digitalWrite(4, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(4, LOW);
+  duration = pulseIn(3, HIGH);
   double distance = ((duration)*(340.0)/(2000));
   Serial.println(distance);
 }
